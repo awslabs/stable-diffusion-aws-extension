71593cc7ee5c5177ec3ea9dad9aae70cd49994f8
AoyuQC <aoyuzhang1989@gmail.com>
2023-05-15 05:50:42 +0000

    fix: rm wrong ext folders

--- extensions/sd_dreambooth_extension/dreambooth/dataclasses/train_result.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-from dreambooth.dataclasses.db_config import DreamboothConfig
-
-
-class TrainResult:
-    config: DreamboothConfig = None
-    msg: str = ""
-    samples: [Image] = []
