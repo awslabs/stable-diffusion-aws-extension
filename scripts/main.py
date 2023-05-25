fba7a16646c8e0eb58d228f1535f79068aea7e2e
Yan Dai <alvindaiyan@gmail.com>
2023-05-25 15:28:59 +0800

    [fix] training job status and checkpoint path error for cloud checkpoint for model creating

--- scripts/main.py
+++ scripts/main.py
@@ -259,7 +259,7 @@ def on_ui_tabs():
                                               outputs=[test_connection_result])
 
                 with gr.Blocks(title="Delete SageMaker Endpoint", variant='panel'):
-                    gr.HTML(value="<u><b>Delete SageMaker Endpoint</b>(Work In Progress)</u>") 
+                    gr.HTML(value="<u><b>Delete SageMaker Endpoint</b>(Work In Progress)</u>")
                     with gr.Row():
                         sagemaker_endpoint_delete_dropdown = gr.Dropdown(choices=sagemaker_ui.sagemaker_endpoints, multiselect=True, label="Select Cloud SageMaker Endpoint")
                         modules.ui.create_refresh_button(sagemaker_endpoint_delete_dropdown, sagemaker_ui.update_sagemaker_endpoints, lambda: {"choices": sagemaker_ui.sagemaker_endpoints}, "refresh_sagemaker_endpoints_delete")
@@ -689,9 +689,11 @@ def async_create_model_on_sagemaker(
             logging.error("Checkpoint name error.")
             return
         params["ckpt_path"] = ckpt_name_list[0].rstrip(".tar")
+        ckpt_info = ckpt_dict[ckpt_key]
         payload = {
             "model_type": "Stable-diffusion",
             "name": new_model_name,
+            "checkpoint_id": ckpt_info["id"],
             "filenames": [],
             "params": {
                 "ckpt_from_cloud": True,
