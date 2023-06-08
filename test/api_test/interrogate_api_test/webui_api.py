3f5817e3ed86d42091855c802556aebfb38cf29b
AoyuQC <aoyuzhang1989@gmail.com>
2023-06-08 05:20:02 +0000

    fix: update interrogate test

--- test/api_test/interrogate_api_test/webui_api.py
+++ test/api_test/interrogate_api_test/webui_api.py
@@ -9,11 +9,17 @@ start_time = time.time()
 
 url = "http://127.0.0.1:7860"
 
+print("webui api test for clip")
+
+with open("test.png", "rb") as img:
+    test_img = base64.b64encode(img.read())
+
 payload = {
-    "image":"",
+    "image":test_img,
     "model":"clip"
 }
 
+# 
 response = requests.post(url=f'{url}/invocations', json=payload)
 
 print(f"run time is {time.time()-start_time}")
@@ -22,14 +28,23 @@ print(f"response is {response}")
 
 r = response.json()
 
-for i in r['images']:
-    image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
+print(f"json response is {r}")
 
-    png_payload = {
-        "image": "data:image/png;base64," + i
-    }
-    response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json=png_payload)
+# for i in r['images']:
+#     image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
 
-    pnginfo = PngImagePlugin.PngInfo()
-    pnginfo.add_text("parameters", response2.json().get("info"))
-    image.save('output.png', pnginfo=pnginfo)
+#     png_payload = {
+#         "image": "data:image/png;base64," + i
+#     }
+#     response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json=png_payload)
+
+#     pnginfo = PngImagePlugin.PngInfo()
+#     pnginfo.add_text("parameters", response2.json().get("info"))
+#     image.save('output.png', pnginfo=pnginfo)
+
+# print("webui api test for deepdanbooru")
+
+# payload = {
+#     "image":"",
+#     "model":"deepdanbooru"
+# }
