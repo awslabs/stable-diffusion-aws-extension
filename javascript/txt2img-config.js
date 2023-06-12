e8d98264db3afb2d5775bcd53b5e467d7c34d83d
guming3d <guming3d@gmail.com>
2023-06-12 04:56:52 +0000

    feat:update the js to get the tab

--- javascript/txt2img-config.js
+++ javascript/txt2img-config.js
@@ -52,7 +52,6 @@ function getDomValue(selector, defaultValue, isTextContent = false) {
 
 // Function to get the selected tab inside the img2img
 function getSelectedButton() {
-    console.log("entering getSelectedButton")
     // Get the parent element
     let parentDiv = document.querySelector("#mode_img2img > div.tab-nav.scroll-hide.svelte-1g805jl");
 
@@ -92,6 +91,43 @@ function getSelectedButton() {
     }
 }
 
+// function to get tab "Restore to" or "Resize by"
+function getSelectedTabResize() {
+    // Get the parent element
+    let parentDiv = document.querySelector("#component-459 > div.tab-nav.scroll-hide.svelte-1g805jl");
+
+    // Get all the button children
+    let buttons = parentDiv.querySelectorAll("button");
+
+    // Initialize a variable to store the selected button
+    let selectedButtonIndex = -1;
+
+    // Loop through each button
+    for (let i = 0; i < buttons.length; i++) {
+        // Check if the button has the 'selected' class
+        if (buttons[i].classList.contains("selected")) {
+            // Store the index of the selected button (add 1 because nth-child is 1-indexed)
+            selectedButtonIndex = i + 1;
+            break;
+        }
+    }
+
+    // Create a mapping from child index to a certain value
+    let mapping = {
+        1: "ResizeTo",
+        2: "ResizeBy"
+    };
+
+    // Check if a button was selected
+    if (selectedButtonIndex != -1) {
+        // If yes, return the corresponding value from the mapping
+        return mapping[selectedButtonIndex];
+    } else {
+        // If no button was selected, return a suitable message
+        return "No tab is selected.";
+    }
+}
+
 
 
 async function txt2img_config_save(endpoint_value) {
@@ -99,14 +135,7 @@ async function txt2img_config_save(endpoint_value) {
 
     console.log(JSON.stringify(endpoint_value))
 
-
-
     scrap_ui_component_value_with_default(config);
-
-
-
-    // store config in local storage for debugging
-    // localStorage.setItem("txt2imgConfig", JSON.stringify(config));
 
     //following code is to get s3 presigned url from middleware and upload the ui parameters
     const key = "config/aigc.json";
@@ -155,11 +184,12 @@ async function img2img_config_save(endpoint_value, init_img, sketch, init_img_wi
 
     config['img2img_selected_tab_name'] = getSelectedButton()
 
-    console.log("guming debug>>")
     console.log(config['img2img_selected_tab_name'])
 
-    // store config in local storage for debugging
-    localStorage.setItem("txt2imgConfig", JSON.stringify(config));
+    config['img2img_selected_resize_tab'] = getSelectedTabResize()
+
+    console.log(config['img2img_selected_resize_tab'])
+
 
     //following code is to get s3 presigned url from middleware and upload the ui parameters
     const key = "config/aigc.json";
