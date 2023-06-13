8066e688069b82bba714abcdbfbe3c3cbe26f45a
AoyuQC <aoyuzhang1989@gmail.com>
2023-06-13 05:36:46 +0000

    feat: add inpaint json

--- test/api_test/img2img_api_test/img2img_local_api.py
+++ test/api_test/img2img_api_test/img2img_local_api.py
@@ -8,6 +8,9 @@ import os
 import sys
 sys.path.append("../../../middleware_api/lambda/inference")
 from parse.parameter_parser import json_convert_to_payload
+from dotenv import load_dotenv
+
+load_dotenv()
 
 start_time = time.time()
 
@@ -15,32 +18,34 @@ start_time = time.time()
 task_type = ''
 payload_checkpoint_info = json.loads(os.environ['checkpoint_info'])
 
-f = open("../json_files/img2img_test.json")
+f = open("../json_files/img2img_inpaint.json")
 
 params_dict = json.load(f)
 
 payload = json_convert_to_payload(params_dict, payload_checkpoint_info, task_type)
 
-# call local api
-url = "http://127.0.0.1:8082"
+print(payload.keys())
 
-response = requests.post(url=f'{url}/sdapi/v1/img2img', json=payload['img2img_payload'])
+# # call local api
+# url = "http://127.0.0.1:8082"
 
-print(f"run time is {time.time()-start_time}")
+# response = requests.post(url=f'{url}/sdapi/v1/img2img', json=payload['img2img_payload'])
 
-print(f"response is {response}")
+# print(f"run time is {time.time()-start_time}")
 
-r = response.json()
-id = 0
-for i in r['images']:
-    image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
+# print(f"response is {response}")
 
-    png_payload = {
-        "image": "data:image/png;base64," + i
-    }
-    response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json=png_payload)
+# r = response.json()
+# id = 0
+# for i in r['images']:
+#     image = Image.open(io.BytesIO(base64.b64decode(i.split(",",1)[0])))
 
-    pnginfo = PngImagePlugin.PngInfo()
-    pnginfo.add_text("parameters", response2.json().get("info"))
-    image.save('output_%d.png'%id, pnginfo=pnginfo)
-    id += 1
+#     png_payload = {
+#         "image": "data:image/png;base64," + i
+#     }
+#     response2 = requests.post(url=f'{url}/sdapi/v1/png-info', json=png_payload)
+
+#     pnginfo = PngImagePlugin.PngInfo()
+#     pnginfo.add_text("parameters", response2.json().get("info"))
+#     image.save('output_%d.png'%id, pnginfo=pnginfo)
+#     id += 1
