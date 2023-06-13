fd19234221bac87bfffb30241360474246d0089e
guming3d <guming3d@gmail.com>
2023-06-13 09:01:07 +0000

    fix: change the s3 policy to retain

--- infrastructure/src/sd-train/sd-train-deploy-stack.ts
+++ infrastructure/src/sd-train/sd-train-deploy-stack.ts
@@ -303,7 +303,7 @@ export class SdTrainDeployStack extends NestedStack {
     //The code that defines your stack goes here
     return new s3.Bucket(this, 'aigc-bucket', {
       blockPublicAccess: BlockPublicAccess.BLOCK_ACLS,
-      removalPolicy: RemovalPolicy.DESTROY,
+      removalPolicy: RemovalPolicy.RETAIN,
       autoDeleteObjects: true,
       cors: corsRules,
     });
