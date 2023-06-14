c84d8128bd902970ae7cc95922bddd684900dd97
AoyuQC <aoyuzhang1989@gmail.com>
2023-06-14 07:05:21 +0000

    docs: img2img on-going

--- test/api_test/interrogate_api_test/interrogate_local_api.py
+++ test/api_test/interrogate_api_test/interrogate_local_api.py
@@ -7,62 +7,68 @@ import sys
 sys.path.append("../../../middleware_api/lambda/inference")
 from parse.parameter_parser import json_convert_to_payload
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 start_time = time.time()
 
 # preapre payload
 task_type = ''
 payload_checkpoint_info = json.loads(os.environ['checkpoint_info'])
 
-f = open("../json_files/img2img_test.json")
+f = open("../json_files/aigc.json")
 
 params_dict = json.load(f)
 
 payload = json_convert_to_payload(params_dict, payload_checkpoint_info, task_type)
 
-# call local api
-url = "http://localhost:8082"
+print(payload.keys())
 
-print("docker api test for clip:")
+# # call local api
+# url = "http://localhost:8082"
 
-with open("test.png", "rb") as img:
-    test_img = str(base64.b64encode(img.read()), 'utf-8')
+# print("docker api test for clip:")
 
-payload = {
-    "task": "interrogate_clip",
-    "interrogate_payload": {
-        "image":test_img,
-        "model":"clip"
-    }
-}
+# with open("test.png", "rb") as img:
+#     test_img = str(base64.b64encode(img.read()), 'utf-8')
 
-# 
-response = requests.post(url=f'{url}/invocations', json=payload)
+# payload = {
+#     "task": "interrogate_clip",
+#     "interrogate_payload": {
+#         "image":test_img,
+#         "model":"clip"
+#     }
+# }
 
-print(f"run time is {time.time()-start_time}")
+# # 
+# response = requests.post(url=f'{url}/invocations', json=payload)
 
-r = response.json()
+# print(f"run time is {time.time()-start_time}")
 
-prompt_message = r["caption"]
+# r = response.json()
 
-print(f"prompt message : {prompt_message}")
+# prompt_message = r["caption"]
 
-print("docker api test for deepbooru:")
+# print(f"prompt message : {prompt_message}")
 
-payload = {
-    "task": "interrogate_deepbooru",
-    "interrogate_payload": {
-        "image":test_img,
-        "model":"deepdanbooru"
-    }
-}
+# print("docker api test for deepbooru:")
 
-# 
-response = requests.post(url=f'{url}/invocations', json=payload)
+# payload = {
+#     "task": "interrogate_deepbooru",
+#     "interrogate_payload": {
+#         "image":test_img,
+#         "model":"deepdanbooru"
+#     }
+# }
 
-print(f"run time is {time.time()-start_time}")
+# # 
+# response = requests.post(url=f'{url}/invocations', json=payload)
 
-r = response.json()
+# print(f"run time is {time.time()-start_time}")
 
-prompt_message = r["caption"]
+# r = response.json()
 
-print(f"prompt message : {prompt_message}")
+# prompt_message = r["caption"]
+
+# print(f"prompt message : {prompt_message}")
