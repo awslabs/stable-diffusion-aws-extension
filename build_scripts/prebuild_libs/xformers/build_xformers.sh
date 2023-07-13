31667bfd557181e082873560e60acfe6b0643dd8
AoyuQC <aoyuzhang1989@gmail.com>
2023-07-13 06:19:46 +0000

    feat: add folders

---
+++ build_scripts/prebuild_libs/xformers/build_xformers.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#FROM 763104351884.dkr.ecr.us-east-1.amazonaws.com/pytorch-inference:2.0.1-gpu-py310-cu118-ubuntu20.04-sagemaker
+
+git clone https://github.com/facebookresearch/xformers.git
+
+cd xformers
+
+git submodule update --init --recursive
+
+pip install -r requirements.txt
+
+python setup.py build
+
+python setup.py bdist_wheel
