5b23cbbf4822716dc9f9b32ab8df07ed33977ab2
Tang Jie <tangjiee@amazon.com>
2023-07-20 05:41:51 +0000

    feat: add dreambooth webui test.

--- test/aigc_webui_dreambooth/test_dreambooth.py
+++ test/aigc_webui_dreambooth/test_dreambooth.py
@@ -16,25 +16,5 @@ def test_dreambooth_create_model():
     elem.click()
     driver.close()
 
-def test_img2img_simple_performed(url_img2img, simple_img2img_request):
-    assert requests.post(url_img2img, json=simple_img2img_request).status_code == 200
-
-
-def test_inpainting_masked_performed(url_img2img, simple_img2img_request, mask_basic_image_base64):
-    simple_img2img_request["mask"] = mask_basic_image_base64
-    assert requests.post(url_img2img, json=simple_img2img_request).status_code == 200
-
-
-def test_inpainting_with_inverted_masked_performed(url_img2img, simple_img2img_request, mask_basic_image_base64):
-    simple_img2img_request["mask"] = mask_basic_image_base64
-    simple_img2img_request["inpainting_mask_invert"] = True
-    assert requests.post(url_img2img, json=simple_img2img_request).status_code == 200
-
-
-def test_img2img_sd_upscale_performed(url_img2img, simple_img2img_request):
-    simple_img2img_request["script_name"] = "sd upscale"
-    simple_img2img_request["script_args"] = ["", 8, "Lanczos", 2.0]
-    assert requests.post(url_img2img, json=simple_img2img_request).status_code == 200
-
 if __name__ == "__main__":
     test_dreambooth_create_model()
