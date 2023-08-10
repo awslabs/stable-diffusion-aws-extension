22dd1e45d5ca844baa14e971646d52de55a11d73
Xiujuan Li <xiujuali@amazon.com>
2023-08-10 14:29:11 +0800

    merge dev

--- scripts/main.py
+++ scripts/main.py
@@ -895,7 +895,7 @@ def on_ui_tabs():
 
                 with gr.Accordion("Upload Model to S3 from Local Path", open=False):
                     gr.HTML(value="Refresh to select the model to upload to S3")
-                    with FormRow(elem_id="model_upload_form_row_01"):
+                    with FormRow(elem_id="model_upload_local_form_row_01"):
                         model_type_drop_down = gr.Dropdown(label="Model Type", choices=["SD Checkpoints", "Textual Inversion", "LoRA model", "ControlNet model", "Hypernetwork", "VAE"], elem_id="model_type_ele_id")
                         model_type_hiden_text = gr.Textbox(elem_id="model_type_value_ele_id", visible=False)
                         def change_model_type_value(model_type: str):
@@ -904,9 +904,9 @@ def on_ui_tabs():
                         model_type_drop_down.change(fn=change_model_type_value, _js="getModelTypeValue",
                                                     inputs=[model_type_drop_down], outputs=model_type_hiden_text)
                         file_upload_html_component = gr.HTML('<div class="lg svelte-1ipelgc"><div class="lg svelte-1ipelgc"><input type="file" class="lg secondary gradio-button svelte-1ipelgc" id="file-uploader" multiple onchange="showFileName(event)" style="width:100%"></div></div>')
-                    with FormRow(elem_id="model_upload_form_row_02"):
+                    with FormRow(elem_id="model_upload_local_form_row_02"):
                         hidden_bind_html = gr.HTML(elem_id="hidden_bind_upload_files", value="<div id='hidden_bind_upload_files_html'></div>")
-                    with FormRow(elem_id="model_upload_form_row_03"):
+                    with FormRow(elem_id="model_upload_local_form_row_03"):
                         upload_label = gr.HTML(label="upload process", elem_id="progress-bar")
                     with gr.Row():
                         model_update_button_local = gr.Button(value="Upload Models to Cloud", variant="primary", elem_id="sagemaker_model_update_button_local", size=(200, 50))
