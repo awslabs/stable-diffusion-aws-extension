4fedf2fa66510f1d066e28af0b058438ef2c362e
AoyuQC <aoyuzhang1989@gmail.com>
2023-11-06 07:27:05 +0000

    feat: update version

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
 
-INITIAL_SUPPORT_COMMIT_ROOT=5ef669de080814067961f28357256e8fe27544f4
-INITIAL_SUPPORT_COMMIT_CONTROLNET=7a4805c8ea3256a0eab3512280bd4f84ca0c8182
-INITIAL_SUPPORT_COMMIT_DREAMBOOTH=c2a5617c587b812b5a408143ddfb18fc49234edf
-INITIAL_SUPPORT_COMMIT_REMBG=3d9eedbbf0d585207f97d5b21e42f32c0042df70
-INITIAL_SUPPORT_COMMIT_SAM=c555c6d9c4e1d14b018e4d2a92acd47765536585
-INITIAL_SUPPORT_COMMIT_TILEDVAE=f9f8073e64f4e682838f255215039ba7884553bf
+set INITIAL_SUPPORT_COMMIT_ROOT=4afaaf8a020c1df457bcf7250cb1c7f609699fa7
+set INITIAL_SUPPORT_COMMIT_CONTROLNET=fce6775a6dddef52ecd658259e909687d9dedf72
+set INITIAL_SUPPORT_COMMIT_DREAMBOOTH=c2a5617c587b812b5a408143ddfb18fc49234edf
+set INITIAL_SUPPORT_COMMIT_REMBG=3d9eedbbf0d585207f97d5b21e42f32c0042df70
+set INITIAL_SUPPORT_COMMIT_SAM=d80220ecd2151cebfb00a5829be8ec1c05118000
+set INITIAL_SUPPORT_COMMIT_TILEDVAE=fbb24736c9bc374c7f098f82b575fcd14a73936a
 
 
 # Clone stable-diffusion-webui
@@ -14,7 +15,7 @@ git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 # Go to stable-diffusion-webui directory
 cd stable-diffusion-webui
 # Reset to specific commit
-git reset --hard ${INITIAL_SUPPORT_COMMIT_ROOT}
+git reset --hard %INITIAL_SUPPORT_COMMIT_ROOT%
 
 # Go to "extensions" directory
 cd extensions
@@ -31,7 +32,7 @@ git clone https://github.com/Mikubill/sd-webui-controlnet.git
 
 # Go to sd-webui-controlnet directory and reset to specific commit
 cd sd-webui-controlnet
-git reset --hard ${INITIAL_SUPPORT_COMMIT_CONTROLNET}
+git reset --hard %INITIAL_SUPPORT_COMMIT_CONTROLNET%
 cd ..
 
 # Clone sd_dreambooth_extension
@@ -39,7 +40,7 @@ git clone https://github.com/d8ahazard/sd_dreambooth_extension.git
 
 # Go to sd_dreambooth_extension directory and reset to specific commit
 cd sd_dreambooth_extension
-git reset --hard ${INITIAL_SUPPORT_COMMIT_DREAMBOOTH}
+git reset --hard %INITIAL_SUPPORT_COMMIT_DREAMBOOTH%
 cd ..
 
 # Clone stable-diffusion-webui-rembg
@@ -47,7 +48,7 @@ git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui-rembg.git
 
 # Go to stable-diffusion-webui-rembg directory and reset to specific commit
 cd stable-diffusion-webui-rembg
-git reset --hard ${INITIAL_SUPPORT_COMMIT_REMBG}
+git reset --hard %INITIAL_SUPPORT_COMMIT_REMBG%
 cd ..
 
 # Clone sd-webui-segment-anything
@@ -55,11 +56,14 @@ git clone https://github.com/continue-revolution/sd-webui-segment-anything.git
 
 # Go to sd-webui-segment-anything directory and reset to specific commit
 cd sd-webui-segment-anything
-git reset --hard ${INITIAL_SUPPORT_COMMIT_SAM}
+git reset --hard %INITIAL_SUPPORT_COMMIT_SAM%
 cd ..
+
 
 # Clone Tiled VAE
 git clone https://github.com/pkuliyi2015/multidiffusion-upscaler-for-automatic1111.git
+
+# Go to multidiffusion-upscaler-for-automatic1111 and reset to specific commit
 cd multidiffusion-upscaler-for-automatic1111
 git reset --hard ${INITIAL_SUPPORT_COMMIT_TILEDVAE}
 cd ..
