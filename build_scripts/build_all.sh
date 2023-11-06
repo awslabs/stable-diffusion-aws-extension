434c54c0af203b68f05b535c6e0bafe47d955c64
AoyuQC <aoyuzhang1989@gmail.com>
2023-11-06 05:43:32 +0000

    feat: update mode to branch

--- build_scripts/build_all.sh
+++ build_scripts/build_all.sh
@@ -4,18 +4,18 @@
 
 # The argument to this script is the image name. This will be used as the image on the local
 # machine and combined with the account and region to form the repository name for ECR.
-mode=$1
+branch=$1
 tag=$2
 commit_id=$3
 
-if [ "$mode" = "" ] || [ "$tag" = "" ] || [ "$commit_id" = "" ]
+if [ "$branch" = "" ] || [ "$tag" = "" ] || [ "$commit_id" = "" ]
 then
     echo "Usage: $0 <extension-branch> <image-tag> <commit_id>"
     exit 1
 fi
 
-./build_and_push.sh Dockerfile.inference.from_scratch aigc-webui-inference $mode $tag $commit_id
+./build_and_push.sh Dockerfile.inference.from_scratch aigc-webui-inference $branch $tag $commit_id
 
-./build_and_push.sh Dockerfile.utils.from_scratch aigc-webui-utils $mode $tag $commit_id
+./build_and_push.sh Dockerfile.utils.from_scratch aigc-webui-utils $branch $tag $commit_id
 
-./build_and_push.sh Dockerfile.dreambooth.from_scratch aigc-webui-dreambooth-training $mode $tag $commit_id
+./build_and_push.sh Dockerfile.dreambooth.from_scratch aigc-webui-dreambooth-training $branch $tag $commit_id
