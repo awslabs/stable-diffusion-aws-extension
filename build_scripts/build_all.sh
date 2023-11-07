72e6f51528705a4969a13889388c61b9da228526
AoyuQC <aoyuzhang1989@gmail.com>
2023-11-07 03:12:59 +0000

    feat: update build sctips to support byoc+diffuses

--- build_scripts/build_all.sh
+++ build_scripts/build_all.sh
@@ -4,18 +4,24 @@
 
 # The argument to this script is the image name. This will be used as the image on the local
 # machine and combined with the account and region to form the repository name for ECR.
-mode=$1
+branch=$1
 tag=$2
 commit_id=$3
 
-if [ "$mode" = "" ] || [ "$tag" = "" ] || [ "$commit_id" = "" ]
+if [ "$branch" = "" ] || [ "$tag" = "" ] || [ "$commit_id" = "" ]
 then
     echo "Usage: $0 <extension-branch> <image-tag> <commit_id>"
     exit 1
 fi
 
-./build_and_push.sh Dockerfile.aigc-endpoint-byoc.from_scratch aigc-endpoint-byoc $mode $tag $commit_id
+./build_and_push.sh Dockerfile.aigc-endpoint-byoc.from_scratch aigc-endpoint-byoc $branch $tag $commit_id
 
-./build_and_push.sh Dockerfile.aigc-endpoint-diffusers.from_scratch aigc-endpoint-diffusers $mode $tag $commit_id
+./build_and_push.sh Dockerfile.aigc-endpoint-diffusers.from_scratch aigc-endpoint-diffusers $branch $tag $commit_id
 
-./build_and_push.sh Dockerfile.aigc-job.from_scratch aigc-job $mode $tag $commit_id
+./build_and_push.sh Dockerfile.aigc-job.from_scratch aigc-job $branch $tag $commit_id
+
+./build_and_push_byoc.sh Dockerfile.aigc-endpoint-byoc.from_scratch aigc-endpoint-byoc $branch $tag $commit_id
+
+./build_and_push_byoc.sh Dockerfile.aigc-endpoint-diffusers.from_scratch aigc-endpoint-diffusers $branch $tag $commit_id
+
+./build_and_push_byoc.sh Dockerfile.aigc-job.from_scratch aigc-job $branch $tag $commit_id
