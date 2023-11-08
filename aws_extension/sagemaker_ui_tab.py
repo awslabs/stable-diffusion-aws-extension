ce9ff34365d89857aa5dc8c9eadf67e6f3888a18
Xiujuan Li <xiujuali@amazon.com>
2023-11-08 10:43:43 +0800

    feat: fix strip

--- aws_extension/sagemaker_ui_tab.py
+++ aws_extension/sagemaker_ui_tab.py
@@ -209,7 +209,7 @@ def user_settings_tab():
 
             def upsert_user(username, password, user_roles, pr: gr.Request):
                 try:
-                    if not username.rstrip() or len(password.strip()) < 1:
+                    if not username.rstrip() or len(username.rstrip()) < 1:
                         return f'Please trim trailing spaces. Username should not be none.'
                     if not password or len(password) < 1:
                         return f'Password should not be none.'
