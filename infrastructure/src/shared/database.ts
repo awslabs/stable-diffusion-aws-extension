55dfbfafc1aa709aabfa09b5e888d6f318b6d89a
Jingyi <elonniu@amazon.com>
2023-12-31 15:12:37 +0800

    improved Construct import

--- infrastructure/src/shared/database.ts
+++ infrastructure/src/shared/database.ts
@@ -1,83 +1,54 @@
-import { aws_dynamodb } from 'aws-cdk-lib';
-import { Attribute, AttributeType } from 'aws-cdk-lib/aws-dynamodb';
-import { Construct } from 'constructs';
-import { ResourceProvider } from './resource-provider';
+import {Construct} from 'constructs';
+import {ResourceProvider} from './resource-provider';
+import {Table} from 'aws-cdk-lib/aws-dynamodb';
 
 
 export class Database {
-  [index: string]: aws_dynamodb.Table;
 
-  constructor(scope: Construct, baseId: string, resourceProvider: ResourceProvider) {
-    interface tableProperties {
-      partitionKey: Attribute;
-      sortKey?: Attribute;
+    public modelTable: Table;
+    public trainingTable: Table;
+    public checkpointTable: Table;
+    public datasetInfoTable: Table;
+    public datasetItemTable: Table;
+    public sDInferenceJobTable: Table;
+    public sDEndpointDeploymentJobTable: Table;
+    public multiUserTable: Table;
+
+    constructor(scope: Construct, baseId: string, resourceProvider: ResourceProvider) {
+
+        this.modelTable = this.table(scope, baseId, 'ModelTable');
+        this.modelTable.node.addDependency(resourceProvider.resources);
+
+        this.trainingTable = this.table(scope, baseId, 'TrainingTable');
+        this.trainingTable.node.addDependency(resourceProvider.resources);
+
+
+        this.checkpointTable = this.table(scope, baseId, 'CheckpointTable');
+        this.checkpointTable.node.addDependency(resourceProvider.resources);
+
+
+        this.datasetInfoTable = this.table(scope, baseId, 'DatasetInfoTable');
+        this.datasetInfoTable.node.addDependency(resourceProvider.resources);
+
+
+        this.datasetItemTable = this.table(scope, baseId, 'DatasetItemTable');
+        this.datasetItemTable.node.addDependency(resourceProvider.resources);
+
+
+        this.sDInferenceJobTable = this.table(scope, baseId, 'SDInferenceJobTable');
+        this.sDInferenceJobTable.node.addDependency(resourceProvider.resources);
+
+
+        this.sDEndpointDeploymentJobTable = this.table(scope, baseId, 'SDEndpointDeploymentJobTable');
+        this.sDEndpointDeploymentJobTable.node.addDependency(resourceProvider.resources);
+
+
+        this.multiUserTable = this.table(scope, baseId, 'MultiUserTable');
+        this.multiUserTable.node.addDependency(resourceProvider.resources);
+
     }
 
-    const tables: { [key: string]: tableProperties } = {
-      ModelTable: {
-        partitionKey: {
-          name: 'id',
-          type: aws_dynamodb.AttributeType.STRING,
-        },
-      },
-      TrainingTable: {
-        partitionKey: {
-          name: 'id',
-          type: aws_dynamodb.AttributeType.STRING,
-        },
-      },
-      CheckpointTable: {
-        partitionKey: {
-          name: 'id',
-          type: aws_dynamodb.AttributeType.STRING,
-        },
-      },
-      DatasetInfoTable: {
-        partitionKey: {
-          name: 'dataset_name',
-          type: aws_dynamodb.AttributeType.STRING,
-        },
-      },
-      DatasetItemTable: {
-        partitionKey: {
-          name: 'dataset_name',
-          type: aws_dynamodb.AttributeType.STRING,
-        },
-        sortKey: {
-          name: 'sort_key',
-          type: AttributeType.STRING,
-        },
-      },
-      SDInferenceJobTable: {
-        partitionKey: {
-          name: 'InferenceJobId',
-          type: aws_dynamodb.AttributeType.STRING,
-        },
-      },
-      SDEndpointDeploymentJobTable: {
-        partitionKey: {
-          name: 'EndpointDeploymentJobId',
-          type: aws_dynamodb.AttributeType.STRING,
-        },
-      },
-      MultiUserTable: {
-        partitionKey: {
-          name: 'kind',
-          type: aws_dynamodb.AttributeType.STRING,
-        },
-        sortKey: {
-          name: 'sort_key',
-          type: AttributeType.STRING,
-        },
-      },
-    };
-
-
-    for (let key in tables) {
-      const tableName = key.charAt(0).toLocaleLowerCase() + key.slice(1);
-      this[tableName] = <aws_dynamodb.Table>aws_dynamodb.Table.fromTableName(scope, `${baseId}-${key}`, key);
-      this[tableName].node.addDependency(resourceProvider.resources);
+    private table(scope: Construct, baseId: string, tableName: string): Table {
+        return <Table>Table.fromTableName(scope, `${baseId}-${tableName}`, tableName);
     }
-
-  }
 }
--- infrastructure/src/shared/s3-bucket.ts
+++ infrastructure/src/shared/s3-bucket.ts
@@ -1,14 +1,14 @@
-import * as s3 from 'aws-cdk-lib/aws-s3';
+import {Bucket} from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 import { ResourceProvider } from './resource-provider';
 
 
 export class S3BucketStore {
-  public readonly s3Bucket: s3.Bucket;
+  public readonly s3Bucket: Bucket;
 
   constructor(scope: Construct, id: string, resourceProvider: ResourceProvider, s3BucketName: string) {
 
-    this.s3Bucket = <s3.Bucket>s3.Bucket.fromBucketName(scope, `${id}-aigc-bucket`, s3BucketName);
+    this.s3Bucket = <Bucket>Bucket.fromBucketName(scope, `${id}-aigc-bucket`, s3BucketName);
     this.s3Bucket.node.addDependency(resourceProvider.resources);
 
   }
