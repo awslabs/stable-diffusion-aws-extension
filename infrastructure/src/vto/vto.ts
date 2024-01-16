f2327c8c0814f10c90860e68596ca69e345dcd84
Jingyi <elonniu@amazon.com>
2024-01-16 16:11:26 +0800

    add v1-5 for ec2

---
+++ infrastructure/src/vto/vto.ts
@@ -0,0 +1,12 @@
+import { NestedStack } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+
+export class VtoStack extends NestedStack {
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+
+  }
+}
