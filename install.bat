1967b5ff13ddeb99d3a571f9f0d6a77f36341102
Jingyi <elonniu@amazon.com>
2024-01-19 22:57:53 +0800

    update install.bat

--- install.bat
+++ install.bat
@@ -1,11 +1,10 @@
 @echo off
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-set INITIAL_SUPPORT_COMMIT_ROOT=bda2ecdbf58fd33b4ad3036ed5cc13eef02747ae
+set INITIAL_SUPPORT_COMMIT_ROOT=cf2772fab0af5573da775e7437e6acdca424f26e
 set INITIAL_SUPPORT_COMMIT_CONTROLNET=7a4805c8ea3256a0eab3512280bd4f84ca0c8182
 set INITIAL_SUPPORT_COMMIT_DREAMBOOTH=c2a5617c587b812b5a408143ddfb18fc49234edf
 set INITIAL_SUPPORT_COMMIT_REMBG=3d9eedbbf0d585207f97d5b21e42f32c0042df70
-set INITIAL_SUPPORT_COMMIT_SAM=c555c6d9c4e1d14b018e4d2a92acd47765536585
 set INITIAL_SUPPORT_COMMIT_TILEDVAE=f9f8073e64f4e682838f255215039ba7884553bf
 
 
@@ -50,15 +49,6 @@ git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui-rembg.git
 cd stable-diffusion-webui-rembg
 git reset --hard %INITIAL_SUPPORT_COMMIT_REMBG%
 cd ..
-
-# Clone sd-webui-segment-anything
-git clone https://github.com/continue-revolution/sd-webui-segment-anything.git
-
-# Go to sd-webui-segment-anything directory and reset to specific commit
-cd sd-webui-segment-anything
-git reset --hard %INITIAL_SUPPORT_COMMIT_SAM%
-cd ..
-
 
 # Clone Tiled VAE
 git clone https://github.com/pkuliyi2015/multidiffusion-upscaler-for-automatic1111.git
