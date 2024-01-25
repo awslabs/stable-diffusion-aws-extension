d42434fcac34de5eda4325d5810bd7a7f7bc5d62
Xiujuan Li <xiujuali@amazon.com>
2024-01-25 16:44:19 +0800

    feat: animatediff support

--- scripts/api.py
+++ scripts/api.py
@@ -136,33 +136,33 @@ def merge_model_on_cloud(req):
     return output_model_position
 
 
-def get_output_img_type(payload: dict) -> str:
+def get_output_img_type(payload: dict):
     try:
         if not payload or 'alwayson_scripts' not in payload.keys() or not payload['alwayson_scripts']:
-            logger.info("not using alwayson_scripts ,image type not set")
+            print("not using alwayson_scripts ,image type not set")
             return None
         # about animatediff out
         if 'animatediff' in payload['alwayson_scripts'].keys() and payload['alwayson_scripts']['animatediff']:
             if ('args' not in payload['alwayson_scripts']['animatediff']
                     or not payload['alwayson_scripts']['animatediff']['args']
                     or not payload['alwayson_scripts']['animatediff']['args'][0]):
-                logger.info("not using alwayson_scripts or args null,image type not set")
+                print("not using alwayson_scripts or args null,image type not set")
                 return None
 
             if ('enable' not in payload['alwayson_scripts']['animatediff']['args'][0]
                     or not payload['alwayson_scripts']['animatediff']['args'][0]['enable']):
-                logger.info("not using alwayson_scripts or not enable ,image type not set")
+                print("not using alwayson_scripts or not enable ,image type not set")
                 return None
 
             if ('format' not in payload['alwayson_scripts']['animatediff']['args'][0]
                     or not payload['alwayson_scripts']['animatediff']['args'][0]['format']):
-                logger.info("not using alwayson_scripts or not set format ,image type not set")
+                print("not using alwayson_scripts or not set format ,image type not set")
                 return None
             images_types = payload['alwayson_scripts']['animatediff']['args'][0]['format']
-            logger.info(f"using alwayson_scripts ,image type set:{images_types}")
+            print(f"using alwayson_scripts ,image type set:{images_types}")
             return images_types
     except Exception as e:
-        logger.error(f"get_output_img_type error:{e}")
+        print(f"get_output_img_type error:{e}")
         return None
 
 
@@ -237,7 +237,7 @@ def sagemaker_api(_, app: FastAPI):
                     image_type = get_output_img_type(payload)
                     if image_type:
                         resp["output_img_type"] = image_type
-                    logger.debug("response", resp)
+                    print("response", resp)
                     return resp
                 elif req.task == 'img2img':
                     logger.info(f"{threading.current_thread().ident}_{threading.current_thread().name}_______ img2img start!!!!!!!!")
@@ -250,7 +250,7 @@ def sagemaker_api(_, app: FastAPI):
                     image_type = get_output_img_type(payload)
                     if image_type:
                         resp["output_img_type"] = image_type
-                    logger.debug("response", resp)
+                    print("response", resp)
                     return resp
                 elif req.task == 'interrogate_clip' or req.task == 'interrogate_deepbooru':
                     response = requests.post(url=f'http://0.0.0.0:8080/sdapi/v1/interrogate',
