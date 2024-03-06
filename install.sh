5aa710b7f5c052a3b570657bc03d6ebabbdf0917
yuxiao <xiaoyu_cindy@msn.com>
2024-03-06 08:48:15 +0000

    build version18 image

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Warning: This script is used to install the initial support for client and workshop
 # Warning: For keeping the same version of the initial support
 
-INITIAL_SUPPORT_COMMIT_ROOT=cf2772fab0af5573da775e7437e6acdca424f26e
+INITIAL_SUPPORT_COMMIT_ROOT=bef51aed032c0aaa5cfd80445bc4cf0d85b408b5
 INITIAL_SUPPORT_COMMIT_CONTROLNET=7a4805c8ea3256a0eab3512280bd4f84ca0c8182
 INITIAL_SUPPORT_COMMIT_TILEDVAE=f9f8073e64f4e682838f255215039ba7884553bf
 INITIAL_SUPPORT_COMMIT_REMBG=3d9eedbbf0d585207f97d5b21e42f32c0042df70
