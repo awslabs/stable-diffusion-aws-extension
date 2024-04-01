6a17dd70927464b4d2ac658cfbe8c4974db1814c
Jingyi Niu <elonniu@amazon.com>
2024-04-01 20:34:12 +0800

    improved inotifywait

--- build_scripts/comfy/inotifywait.sh
+++ build_scripts/comfy/inotifywait.sh
@@ -16,17 +16,18 @@ sync_files(){
   s5cmd sync "/ComfyUI/input/*" "s3://$COMFY_BUCKET_NAME/comfy/$COMFY_ENDPOINT/$timestamp/input/"
   s5cmd sync "/ComfyUI/models/*" "s3://$COMFY_BUCKET_NAME/comfy/$COMFY_ENDPOINT/$timestamp/models/"
   s5cmd sync "/ComfyUI/custom_nodes/*" "s3://$COMFY_BUCKET_NAME/comfy/$COMFY_ENDPOINT/$timestamp/custom_nodes/"
-  curl --location --request POST "${COMFY_API_URL}prepare" \
-  --header "x-api-key: $COMFY_API_TOKEN" \
-  --data-raw "{
-      \"endpoint_name\":\"$COMFY_ENDPOINT\",
-      \"need_reboot\": true,
-      \"prepare_id\": \"$timestamp\",
-      \"prepare_type\": \"default\"
-  }"
+  url="${COMFY_API_URL}prepare"
+  echo "URL: $url"
+  data="{\"endpoint_name\":\"$COMFY_ENDPOINT\", \"need_reboot\": true, \"prepare_id\": \"$timestamp\"}"
+  echo "Data: "
+  echo "$data" | jq
+  result=$(curl --location --request POST "$url" --header "x-api-key: $COMFY_API_TOKEN" --data-raw "$data")
+  echo "$result" | jq
 }
 
 inotifywait -m -e modify,create,delete --format '%w %e' "$DIR1" "$DIR2" "$DIR3" |
     while read -r directory events; do
         sync_files "$directory" "$events"
     done
+
+# sudo systemctl restart comfy_upload.service
