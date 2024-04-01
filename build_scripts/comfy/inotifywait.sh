a6309b814189f17060639e8b3159060d2e8a57ea
Jingyi Niu <elonniu@amazon.com>
2024-04-01 18:09:27 +0800

    improved inotifywait

--- build_scripts/comfy/inotifywait.sh
+++ build_scripts/comfy/inotifywait.sh
@@ -16,6 +16,14 @@ sync_files(){
   s5cmd sync "/ComfyUI/input/*" "s3://$COMFY_BUCKET_NAME/comfy/$COMFY_ENDPOINT/$timestamp/input/"
   s5cmd sync "/ComfyUI/models/*" "s3://$COMFY_BUCKET_NAME/comfy/$COMFY_ENDPOINT/$timestamp/models/"
   s5cmd sync "/ComfyUI/custom_nodes/*" "s3://$COMFY_BUCKET_NAME/comfy/$COMFY_ENDPOINT/$timestamp/custom_nodes/"
+  curl --location --request POST "${COMFY_API_URL}prepare" \
+  --header "x-api-key: $COMFY_API_TOKEN" \
+  --data-raw "{
+      \"endpoint_name\":\"$COMFY_ENDPOINT\",
+      \"need_reboot\": true,
+      \"prepare_id\": \"$timestamp\",
+      \"prepare_type\": \"default\"
+  }"
 }
 
 inotifywait -m -e modify,create,delete --format '%w %e' "$DIR1" "$DIR2" "$DIR3" |
