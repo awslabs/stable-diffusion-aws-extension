a48945e10e59dfa69459f1219d98cd5628f567ea
Xiujuan Li <xiujuali@amazon.com>
2024-04-03 13:26:47 +0800

    fix proxy and msg

--- build_scripts/comfy/serve.py
+++ build_scripts/comfy/serve.py
@@ -87,20 +87,17 @@ class ComfyApp:
         self.start()
 
 
-# def start_comfy_app(host=LOCALHOST, port=COMFY_PORT):
-#     cmd = "python main.py  --listen {} --port {}".format(host, port)
-#     os.system(cmd)
 def check_sync():
     logger.info("start check_sync!")
     while True:
         try:
-            # logger.info("start check_sync! checking function-------")
+            logger.info("start check_sync! checking function-------")
             response = requests.post(f"http://{PHY_LOCALHOST}:{COMFY_PORT}/sync_instance")
-            # logger.info(f"sync response:{response.json()} time : {datetime.datetime.now()}")
+            logger.info(f"sync response:{response.json()} time : {datetime.datetime.now()}")
 
-            # logger.info("start check_reboot! checking function-------")
+            logger.info("start check_reboot! checking function-------")
             response2 = requests.post(f"http://{PHY_LOCALHOST}:{COMFY_PORT}/reboot")
-            # logger.info(f"reboot response:{response.json()} time : {datetime.datetime.now()}")
+            logger.info(f"reboot response:{response.json()} time : {datetime.datetime.now()}")
             time.sleep(SLEEP_TIME)
         except Exception as e:
             logger.info(f"check_and_reboot error:{e}")
