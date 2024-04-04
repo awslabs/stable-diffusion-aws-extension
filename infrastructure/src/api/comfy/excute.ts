cb829c511b41ac7cfece6c22125685b9b4c77cb4
Jingyi Niu <elonniu@amazon.com>
2024-04-04 19:24:01 +0800

    improved lambda files

--- infrastructure/src/api/comfy/excute.ts
+++ infrastructure/src/api/comfy/excute.ts
@@ -170,7 +170,6 @@ export class ExecuteApi {
               'sagemaker_raw',
               'output_path',
             ],
-            additionalProperties: false,
           },
         },
         required: [
