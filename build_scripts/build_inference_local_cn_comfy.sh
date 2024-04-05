e3d9abc695e6715a3a8a7d6fd4e06f92a554862d
Jingyi <elonniu@amazon.com>
2024-04-05 16:10:50 +0800

    update cn dockerfile

--- build_scripts/build_inference_local_cn_comfy.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-./build_and_push_local.sh Dockerfile.inference.cn.comfy.from_scratch esd-inference dev "comfy-cn"
--- build_scripts/build_inference_local_cn_sd.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-./build_and_push_local.sh Dockerfile.inference.cn.sd.from_scratch esd-inference dev "sd-cn"
