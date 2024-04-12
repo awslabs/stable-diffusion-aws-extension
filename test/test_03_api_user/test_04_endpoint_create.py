0da4764e697961a394e4497219a79194a9e50a7d
Jingyi Niu <elonniu@amazon.com>
2024-04-12 10:19:22 +0800

    improved test

--- test/test_03_api_user/test_04_endpoint_create.py
+++ test/test_03_api_user/test_04_endpoint_create.py
@@ -126,7 +126,7 @@ class TestEndpointCreateE2E:
         assert 'data' in resp.json(), resp.dumps()
         assert resp.json()["data"]["endpoint_status"] == "Creating", resp.dumps()
 
-    def test_5_create_endpoint_exists(self):
+    def test_5_create_sd_endpoint_exists(self):
         headers = {
             "x-api-key": config.api_key,
             "username": config.username
@@ -145,7 +145,7 @@ class TestEndpointCreateE2E:
         resp = self.api.create_endpoint(headers=headers, data=data)
         assert "Cannot create already existing model" in resp.json()["message"]
 
-    def test_3_create_endpoint_async_for_comfy(self):
+    def test_3_create_confy_endpoint_async(self):
         headers = {
             "x-api-key": config.api_key,
             "username": config.username
@@ -158,7 +158,7 @@ class TestEndpointCreateE2E:
             "instance_type": config.async_instance_type,
             "initial_instance_count": 1,
             "autoscaling_enabled": True,
-            "assign_to_roles": [config.role_sd_async],
+            "assign_to_roles": [config.role_comfy_async],
             "creator": config.username
         }
 
@@ -166,7 +166,7 @@ class TestEndpointCreateE2E:
         assert 'data' in resp.json(), resp.dumps()
         assert resp.json()["data"]["endpoint_status"] == "Creating", resp.dumps()
 
-    def test_4_create_endpoint_real_time_for_comfy(self):
+    def test_4_create_comfy_endpoint_real_time(self):
         headers = {
             "x-api-key": config.api_key,
             "username": config.username
@@ -192,6 +192,7 @@ class TestEndpointCreateE2E:
             "x-api-key": config.api_key,
             "username": config.username
         }
+
         data = {
             "endpoint_name": "test",
             "endpoint_type": 'Async',
