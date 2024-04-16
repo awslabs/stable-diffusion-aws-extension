887ab00afea48bd99d05ada20ac4ee0ef7fb0631
Jingyi <elonniu@amazon.com>
2024-04-16 18:00:02 +0800

    improved export tag name

--- build_scripts/build_and_export_files.sh
+++ build_scripts/build_and_export_files.sh
@@ -11,10 +11,10 @@ echo "docker build $tag_name"
 docker build --build-arg ESD_COMMIT_ID="$CODEBUILD_RESOLVED_SOURCE_VERSION" -t "$tag_name" -f "$dockerfile" .
 docker images "$tag_name"
 
-docker create --name file "$tag_name"
+docker create --name "$tag_name" "$tag_name"
 rm -rf  "/tmp/$tag_name"
 mkdir -p "/tmp/$tag_name"
-docker cp file:/home/ubuntu "/tmp/$tag_name/"
+docker cp "$tag_name:/home/ubuntu" "/tmp/$tag_name/"
 tar -cf ubuntu.tar -C "/tmp/$tag_name/ubuntu" . > /dev/null 2>&1
 ls -la
 
@@ -22,4 +22,4 @@ mkdir -p ~/.aws
 echo "[default]
 region = $REGION" > ~/.aws/config
 
-s5cmd cp ubuntu.tar "s3://aws-gcr-solutions-$REGION/stable-diffusion-aws-extension-github-mainline/$BUILD_VERSION/ubuntu.tar"
+s5cmd sync ubuntu.tar "s3://aws-gcr-solutions-$REGION/stable-diffusion-aws-extension-github-mainline/$BUILD_VERSION/ubuntu.tar"
