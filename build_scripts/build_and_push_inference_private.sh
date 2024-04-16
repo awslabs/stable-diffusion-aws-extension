88918a5cf1f17ddef5e38a5595bea0f60370a138
Jingyi <elonniu@amazon.com>
2024-04-16 17:17:25 +0800

    export fullname

--- build_scripts/build_and_push_inference_private.sh
+++ build_scripts/build_and_push_inference_private.sh
@@ -54,7 +54,7 @@ aws ecr get-login-password --region "$region" | docker login --username AWS --pa
 
 # Build the docker image locally with the image name and then push it to ECR
 # with the full name.
-fullname="$account.dkr.ecr.$region.$AWS_DOMAIN/$repo_name:$tag"
+export fullname="$account.dkr.ecr.$region.$AWS_DOMAIN/$repo_name:$tag"
 echo "docker build $fullname"
 docker build --build-arg ESD_COMMIT_ID="$CODEBUILD_RESOLVED_SOURCE_VERSION" -t "$fullname" -f "$dockerfile" .
 docker images
