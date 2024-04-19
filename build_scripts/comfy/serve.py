c3f28e9bce3ceb8cebed20f6ae14f9dea3b1abac
Xiujuan Li <xiujuali@amazon.com>
2024-04-19 22:25:35 +0800

    debug endpoint

--- build_scripts/comfy/serve.py
+++ build_scripts/comfy/serve.py
@@ -66,7 +66,6 @@ class Worker(multiprocessing.Process):
         while True:
             try:
                 job = self.task_queue.get(timeout=1)
-                # Create a new process for the job
                 process = multiprocessing.Process(target=self.process_job, args=(job,))
                 process.start()
                 while True:
@@ -95,15 +94,15 @@ class Worker(multiprocessing.Process):
 
     def process_job(self, job):
         try:
-            # time.sleep(random.uniform(1, 5))
             comfy_app = job.serve_app
             req = job.req
+            logger.info(f"process_job start : {comfy_app} {req}")
             if comfy_app is None:
                 raise HTTPException(status_code=500,
                                     detail=f"COMFY service returned an error: no avaliable app")
             response = requests.post(f"http://{PHY_LOCALHOST}:{comfy_app.port}/execute_proxy", json=req)
+            logger.info(f"process_job end {response.json()}")
             return response.json()
-
         except Exception as e:
             job.error = str(e)
 
@@ -154,9 +153,10 @@ class ProcessLifecycleController:
                 job = self.result_queue.get(timeout=1)
                 if job.state == ProcessState.ZOMBIE:
                     if job.error is None:
+                        logger.info(f"Job {job.job_id} is completed")
                         completed_jobs.append(job.job_id)
                     else:
-                        print(f"Job {job.job_id} failed: {job.error}")
+                        logger.info(f"Job {job.job_id} failed: {job.error}")
                         if job.job_id in self.jobs:
                             del self.jobs[job.job_id]
             except queue.Empty:
@@ -176,43 +176,71 @@ async def invocations(request: Request):
     global is_multi_gpu
     if is_multi_gpu:
         gpu_nums = get_gpu_count()
-        controller = ProcessLifecycleController(num_workers=gpu_nums)
-        controller.start()
-        job_ids = []
-        req_list = await request.json()
-        result_list = []
+        logger.info(f"Number of GPUs: {gpu_nums}")
+        req = await request.json()
+        logger.info(f"Starting single invocation {req}")
+        result = []
+        for request_obj in req:
+            comfy_app = get_available_app()
+            i=0
+            while comfy_app is None:
+                comfy_app = get_available_app()
+                if comfy_app is None:
+                    time.sleep(1)
+                    i = i + 1
+                    if i >= 3:
+                        logger.info(f"there is no available comfy_app for {i}")
+                        break
+            if comfy_app is None:
+                logger.info(f"there is no available comfy_app! ignore this request is {req}")
+                continue
+            logger.info(f"invocations start req:{request_obj}  url:{PHY_LOCALHOST}:{comfy_app.port}/invocations")
+            response = requests.post(f"http://{PHY_LOCALHOST}:{comfy_app.port}/invocations", json=request_obj)
+            if response.status_code != 200:
+                raise HTTPException(status_code=response.status_code,
+                                    detail=f"COMFY service returned an error: {response.text}")
+            result.append(response.json())
+        return result
 
-        for request_obj in req_list:
-            job_id = controller.submit_job(request_obj, get_available_app())
-            job_ids.append(job_id)
-        # Monitor job status and results
-        while job_ids:
-            for job_id in job_ids:
-                status = controller.get_job_status(job_id)
-                print(f"Job {job_id} status: {status}")
-                if status == ProcessState.ZOMBIE:
-                    result = controller.get_job_result(job_id)
-                    if result is not None:
-                        result_list.append(result)
-                        print(f"Job {job_id} completed. Result: {result}")
-                        job_ids.remove(job_id)
-                    else:
-                        error = controller.get_job_error(job_id)
-                        print(f"Job {job_id} failed. Error: {error}")
-                        job_ids.remove(job_id)
-                elif status is None:
-                    print(f"Job {job_id} status is None")
-                    job_ids.remove(job_id)
-            controller.monitor_jobs()
-            # time.sleep(1)
-        controller.stop()
-        return result_list
+        # controller = ProcessLifecycleController(num_workers=gpu_nums)
+        # controller.start()
+        # job_ids = []
+        # req_list = await request.json()
+        # result_list = []
+        #
+        # for request_obj in req_list:
+        #     job_id = controller.submit_job(request_obj, get_available_app())
+        #     job_ids.append(job_id)
+        # logger.info(f"submit_job commit ids: {job_ids}")
+        # # Monitor job status and results
+        # while job_ids:
+        #     for job_id in job_ids:
+        #         status = controller.get_job_status(job_id)
+        #         logger.info(f"Job {job_id} status: {status}")
+        #         if status == ProcessState.ZOMBIE:
+        #             result = controller.get_job_result(job_id)
+        #             if result is not None:
+        #                 result_list.append(result)
+        #                 logger.info(f"Job {job_id} completed. Result: {result}")
+        #                 job_ids.remove(job_id)
+        #             else:
+        #                 error = controller.get_job_error(job_id)
+        #                 logger.info(f"Job {job_id} failed. Error: {error}")
+        #                 job_ids.remove(job_id)
+        #         elif status is None:
+        #             logger.info(f"Job {job_id} status is None")
+        #             job_ids.remove(job_id)
+        #     controller.monitor_jobs()
+        #     # time.sleep(1)
+        # controller.stop()
+        # return result_list
     else:
         comfy_app = get_available_app()
         req = await request.json()
+        logger.info(f"Starting single invocation on {comfy_app.port} {req}")
         result = []
         for request_obj in req:
-            logger.info(f"invocations start req:{req}  url:{PHY_LOCALHOST}:{comfy_app.port}/invocations")
+            logger.info(f"invocations start req:{request_obj}  url:{PHY_LOCALHOST}:{comfy_app.port}/invocations")
             response = requests.post(f"http://{PHY_LOCALHOST}:{comfy_app.port}/invocations", json=request_obj)
             if response.status_code != 200:
                 raise HTTPException(status_code=response.status_code,
@@ -303,10 +331,10 @@ def get_gpu_count():
         gpu_count = result.stdout.count('\n')
         return gpu_count
     except subprocess.CalledProcessError as e:
-        print("Failed to run nvidia-smi:", e)
+        logger.info("Failed to run nvidia-smi:", e)
         return 0
     except Exception as e:
-        print("An error occurred:", e)
+        logger.info("An error occurred:", e)
         return 0
 
 
