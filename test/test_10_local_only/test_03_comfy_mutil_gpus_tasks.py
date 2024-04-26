a59ef738aad084eb6e859e1d10447b1d3c1b8fda
Jingyi <elonniu@amazon.com>
2024-04-26 15:03:49 +0800

    update tests

--- test/test_10_local_only/test_03_comfy_mutil_gpus_tasks.py
+++ test/test_10_local_only/test_03_comfy_mutil_gpus_tasks.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 import json
 import logging
 import os
+import threading
 import time
 import uuid
 from datetime import datetime, timedelta
@@ -30,163 +31,78 @@ def tps_async_create(n, api, endpoint_name):
         "x-api-key": config.api_key,
     }
 
-    with open("./data/api_params/comfy_workflow.json", 'rb') as data:
-        file_content = data.read()
-        file_content = json.loads(file_content)
-        print(file_content)
+    # with open("./data/api_params/comfy_workflow.json", 'rb') as data:
+    #     file_content = data.read()
+    #     file_content = json.loads(file_content)
+    #     print(file_content)
 
     prompt_id = str(uuid.uuid4())
 
-    payload = json.dumps({
-        "need_sync": True,
-        "prompt": {
-            "4": {
-                "inputs": {
-                    "ckpt_name": "sdXL_v10VAEFix.safetensors"
-                },
-                "class_type": "CheckpointLoaderSimple"
-            },
-            "5": {
-                "inputs": {
-                    "width": 1024,
-                    "height": 1024,
-                    "batch_size": 1
-                },
-                "class_type": "EmptyLatentImage"
-            },
-            "6": {
-                "inputs": {
-                    "text": "evening sunset scenery blue sky nature, glass bottle with a galaxy in it",
-                    "clip": [
-                        "4",
-                        1
-                    ]
-                },
-                "class_type": "CLIPTextEncode"
-            },
-            "7": {
-                "inputs": {
-                    "text": "text, watermark",
-                    "clip": [
-                        "4",
-                        1
-                    ]
-                },
-                "class_type": "CLIPTextEncode"
-            },
-            "10": {
-                "inputs": {
-                    "add_noise": "enable",
-                    "noise_seed": 721897303308196,
-                    "steps": 25,
-                    "cfg": 8,
-                    "sampler_name": "euler",
-                    "scheduler": "normal",
-                    "start_at_step": 0,
-                    "end_at_step": 20,
-                    "return_with_leftover_noise": "enable",
-                    "model": [
-                        "4",
-                        0
-                    ],
-                    "positive": [
-                        "6",
-                        0
-                    ],
-                    "negative": [
-                        "7",
-                        0
-                    ],
-                    "latent_image": [
-                        "5",
-                        0
-                    ]
-                },
-                "class_type": "KSamplerAdvanced"
-            },
-            "11": {
-                "inputs": {
-                    "add_noise": "disable",
-                    "noise_seed": 0,
-                    "steps": 25,
-                    "cfg": 8,
-                    "sampler_name": "euler",
-                    "scheduler": "normal",
-                    "start_at_step": 20,
-                    "end_at_step": 10000,
-                    "return_with_leftover_noise": "disable",
-                    "model": [
-                        "12",
-                        0
-                    ],
-                    "positive": [
-                        "15",
-                        0
-                    ],
-                    "negative": [
-                        "16",
-                        0
-                    ],
-                    "latent_image": [
-                        "10",
-                        0
-                    ]
-                },
-                "class_type": "KSamplerAdvanced"
-            },
-            "12": {
-                "inputs": {
-                    "ckpt_name": "sdXL_v10VAEFix.safetensors"
-                },
-                "class_type": "CheckpointLoaderSimple"
-            },
-            "15": {
-                "inputs": {
-                    "text": "evening sunset scenery blue sky nature, glass bottle with a galaxy in it",
-                    "clip": [
-                        "12",
-                        1
-                    ]
-                },
-                "class_type": "CLIPTextEncode"
-            },
-            "16": {
-                "inputs": {
-                    "text": "text, watermark",
-                    "clip": [
-                        "12",
-                        1
-                    ]
-                },
-                "class_type": "CLIPTextEncode"
-            },
-            "17": {
-                "inputs": {
-                    "samples": [
-                        "11",
-                        0
-                    ],
-                    "vae": [
-                        "12",
-                        2
-                    ]
-                },
-                "class_type": "VAEDecode"
-            },
-            "19": {
-                "inputs": {
-                    "filename_prefix": "ComfyUI",
-                    "images": [
-                        "17",
-                        0
-                    ]
-                },
-                "class_type": "SaveImage"
-            }
-        },
-        "prompt_id": prompt_id,
-        "endpoint_name": endpoint_name
-    })
+    payload = json.dumps({'number': '1', 'prompt': {'3': {
+        'inputs': {'seed': 156680208700286, 'steps': 20, 'cfg': 8.0, 'sampler_name': 'euler', 'scheduler': 'normal',
+                   'denoise': 1.0, 'model': ['4', 0], 'positive': ['6', 0], 'negative': ['7', 0],
+                   'latent_image': ['5', 0]}, 'class_type': 'KSampler', '_meta': {'title': 'KSampler'}}, '4': {
+        'inputs': {'ckpt_name': 'v1-5-pruned-emaonly.ckpt'},
+        'class_type': 'CheckpointLoaderSimple', '_meta': {'title': 'Load Checkpoint'}},
+        '5': {'inputs': {'width': 512, 'height': 512, 'batch_size': 1},
+              'class_type': 'EmptyLatentImage',
+              '_meta': {'title': 'Empty Latent Image'}}, '6': {
+            'inputs': {'text': 'beautiful scenery nature glass bottle landscape, , purple galaxy bottle,',
+                       'clip': ['4', 1]}, 'class_type': 'CLIPTextEncode',
+            '_meta': {'title': 'CLIP Text Encode (Prompt)'}},
+        '7': {'inputs': {'text': 'text, watermark', 'clip': ['4', 1]},
+              'class_type': 'CLIPTextEncode',
+              '_meta': {'title': 'CLIP Text Encode (Prompt)'}},
+        '8': {'inputs': {'samples': ['3', 0], 'vae': ['4', 2]},
+              'class_type': 'VAEDecode', '_meta': {'title': 'VAE Decode'}},
+        '9': {'inputs': {'filename_prefix': 'ComfyUI', 'images': ['8', 0]},
+              'class_type': 'SaveImage', '_meta': {'title': 'Save Image'}}},
+                          'prompt_id': prompt_id, 'extra_data': {'extra_pnginfo': {
+            'workflow': {'last_node_id': 9, 'last_link_id': 9, 'nodes': [
+                {'id': 7, 'type': 'CLIPTextEncode', 'pos': [413, 389],
+                 'size': {'0': 425.27801513671875, '1': 180.6060791015625}, 'flags': {}, 'order': 3, 'mode': 0,
+                 'inputs': [{'name': 'clip', 'type': 'CLIP', 'link': 5}],
+                 'outputs': [{'name': 'CONDITIONING', 'type': 'CONDITIONING', 'links': [6], 'slot_index': 0}],
+                 'properties': {'Node name for S&R': 'CLIPTextEncode'}, 'widgets_values': ['text, watermark']},
+                {'id': 6, 'type': 'CLIPTextEncode', 'pos': [415, 186],
+                 'size': {'0': 422.84503173828125, '1': 164.31304931640625}, 'flags': {}, 'order': 2, 'mode': 0,
+                 'inputs': [{'name': 'clip', 'type': 'CLIP', 'link': 3}],
+                 'outputs': [{'name': 'CONDITIONING', 'type': 'CONDITIONING', 'links': [4], 'slot_index': 0}],
+                 'properties': {'Node name for S&R': 'CLIPTextEncode'},
+                 'widgets_values': ['beautiful scenery nature glass bottle landscape, , purple galaxy bottle,']},
+                {'id': 5, 'type': 'EmptyLatentImage', 'pos': [473, 609], 'size': {'0': 315, '1': 106}, 'flags': {},
+                 'order': 0, 'mode': 0,
+                 'outputs': [{'name': 'LATENT', 'type': 'LATENT', 'links': [2], 'slot_index': 0}],
+                 'properties': {'Node name for S&R': 'EmptyLatentImage'}, 'widgets_values': [512, 512, 1]},
+                {'id': 3, 'type': 'KSampler', 'pos': [863, 186], 'size': {'0': 315, '1': 262}, 'flags': {}, 'order': 4,
+                 'mode': 0, 'inputs': [{'name': 'model', 'type': 'MODEL', 'link': 1},
+                                       {'name': 'positive', 'type': 'CONDITIONING', 'link': 4},
+                                       {'name': 'negative', 'type': 'CONDITIONING', 'link': 6},
+                                       {'name': 'latent_image', 'type': 'LATENT', 'link': 2}],
+                 'outputs': [{'name': 'LATENT', 'type': 'LATENT', 'links': [7], 'slot_index': 0}],
+                 'properties': {'Node name for S&R': 'KSampler'},
+                 'widgets_values': [156680208700286, 'randomize', 20, 8, 'euler', 'normal', 1]},
+                {'id': 8, 'type': 'VAEDecode', 'pos': [1209, 188], 'size': {'0': 210, '1': 46}, 'flags': {}, 'order': 5,
+                 'mode': 0, 'inputs': [{'name': 'samples', 'type': 'LATENT', 'link': 7},
+                                       {'name': 'vae', 'type': 'VAE', 'link': 8}],
+                 'outputs': [{'name': 'IMAGE', 'type': 'IMAGE', 'links': [9], 'slot_index': 0}],
+                 'properties': {'Node name for S&R': 'VAEDecode'}},
+                {'id': 9, 'type': 'SaveImage', 'pos': [1451, 189], 'size': {'0': 210, '1': 58}, 'flags': {}, 'order': 6,
+                 'mode': 0, 'inputs': [{'name': 'images', 'type': 'IMAGE', 'link': 9}], 'properties': {},
+                 'widgets_values': ['ComfyUI']},
+                {'id': 4, 'type': 'CheckpointLoaderSimple', 'pos': [26, 474], 'size': {'0': 315, '1': 98}, 'flags': {},
+                 'order': 1, 'mode': 0, 'outputs': [{'name': 'MODEL', 'type': 'MODEL', 'links': [1], 'slot_index': 0},
+                                                    {'name': 'CLIP', 'type': 'CLIP', 'links': [3, 5], 'slot_index': 1},
+                                                    {'name': 'VAE', 'type': 'VAE', 'links': [8], 'slot_index': 2}],
+                 'properties': {'Node name for S&R': 'CheckpointLoaderSimple'},
+                 'widgets_values': ['v1-5-pruned-emaonly.ckpt']}],
+                         'links': [[1, 4, 0, 3, 0, 'MODEL'], [2, 5, 0, 3, 3, 'LATENT'], [3, 4, 1, 6, 0, 'CLIP'],
+                                   [4, 6, 0, 3, 1, 'CONDITIONING'], [5, 4, 1, 7, 0, 'CLIP'],
+                                   [6, 7, 0, 3, 2, 'CONDITIONING'], [7, 3, 0, 8, 0, 'LATENT'], [8, 4, 2, 8, 1, 'VAE'],
+                                   [9, 8, 0, 9, 0, 'IMAGE']], 'groups': [], 'config': {}, 'extra': {}, 'version': 0.4}},
+            'client_id': '6cbc8da8bb4a4012b9e40b1eae406556'},
+                          'endpoint_name': endpoint_name, 'need_prepare': False, 'need_sync': False,
+                          'multi_async': True})
 
     resp = api.create_execute(headers=headers, data=json.loads(payload))
     assert resp.status_code == 201, resp.dumps()
@@ -195,8 +111,6 @@ def tps_async_create(n, api, endpoint_name):
     logger.info(f"{n} prompt_id is {prompt_id}")
 
     assert resp.json()["statusCode"] == 201
-
-    return
 
     prompt_id = inference_data["prompt_id"]
 
@@ -228,14 +142,14 @@ class TestComfyMutilTaskGPUs:
         pass
 
     def test_10_download_file(self):
-        local_path = f"./data/comfy/checkpoints/v1-5-pruned-emaonly.ckpt"
+        local_path = f"./data/comfy/models/checkpoints/v1-5-pruned-emaonly.ckpt"
         wget_file(
             local_path,
             'https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt'
         )
 
     def test_11_sync_files_to_comfy_endpoint(self):
-        local = "'./data/comfy/*'"
+        local = "'./data/comfy/models/*'"
         target = f"'s3://{config.bucket}/comfy/{self.endpoint_name}/{id}/models/'"
         logger.info(f"Syncing {local} to {target}")
         os.system(f"rm -rf ./s5cmd")
@@ -244,24 +158,62 @@ class TestComfyMutilTaskGPUs:
         os.system(f"./s5cmd sync {local} {target}")
 
     def test_12_sync_files(self):
+        headers = {
+            "x-api-key": config.api_key,
+            "username": config.username
+        }
         data = {"endpoint_name": f"{self.endpoint_name}",
                 "need_reboot": True,
                 "prepare_id": id,
                 "prepare_type": "models"}
-        self.api.prepare(data=data)
-
-    def test_13_comfy_gpus_clear_inferences_jobs(self):
-        resp = self.api.list_inferences(headers, params={"limit": 100})
+        resp = self.api.prepare(data=data, headers=headers)
         assert resp.status_code == 200, resp.dumps()
-        inferences = resp.json()['data']['inferences']
-        list = []
-        for inference in inferences:
-            list.append(inference['InferenceJobId'])
-        data = {
-            "inference_id_list": list
+        logger.info(resp.json())
+        logger.info(f"wait 30s for endpoint sync files...")
+        time.sleep(30)
+
+    def test_13_clean_all_executes(self):
+        headers = {
+            "x-api-key": config.api_key,
+            "username": config.username
         }
-        self.api.delete_inferences(data=data, headers=headers)
+
+        while True:
+
+            resp = self.api.list_executes(headers=headers)
+            executes = resp.json()['data']['executes']
+            if len(executes) == 0:
+                break
+
+            for execute in executes:
+                prompt_id = execute['prompt_id']
+                data = {
+                    "execute_id_list": [
+                        prompt_id
+                    ],
+                }
+                resp = self.api.delete_executes(headers=headers, data=data)
+                logger.info(f"delete execute {prompt_id}")
+                if resp.status_code == 400:
+                    logger.info(resp.json()['message'])
+                    time.sleep(5)
+                    continue
 
     def test_14_comfy_gpus_start_async_tps(self):
-        for n in range(4000):
-            tps_async_create(n, self.api, self.endpoint_name)
+        threads = []
+        gpus = 1
+        batch = 1
+        for i in range(gpus):
+            thread = threading.Thread(target=create_batch_executes, args=(batch, self.api, self.endpoint_name))
+            threads.append(thread)
+
+        for thread in threads:
+            thread.start()
+
+        for thread in threads:
+            thread.join()
+
+
+def create_batch_executes(n, api, endpoint_name):
+    for i in range(n):
+        tps_async_create(i, api, endpoint_name)
