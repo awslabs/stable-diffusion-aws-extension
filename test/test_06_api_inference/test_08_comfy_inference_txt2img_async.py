490e490a1c238f04246bbd8bca8522dd479f0dba
Jingyi Niu <elonniu@amazon.com>
2024-04-26 16:08:36 +0800

    improved gpus tasks

--- test/test_06_api_inference/test_08_comfy_inference_txt2img_async.py
+++ test/test_06_api_inference/test_08_comfy_inference_txt2img_async.py
@@ -1,8 +1,11 @@
 import logging
+import os
+import time
 
 import config as config
 from utils.api import Api
 from utils.helper import update_oas, comfy_execute_create
+from utils.helper import wget_file
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +19,37 @@ class TestTxt2ImgInferenceAsyncAfterComfyE2E:
     @classmethod
     def teardown_class(self):
         pass
+
+    def test_0_download_file(self):
+        local_path = f"./data/comfy/models/checkpoints/v1-5-pruned-emaonly.ckpt"
+        wget_file(
+            local_path,
+            'https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt'
+        )
+
+    def test_1_sync_files_to_comfy_endpoint(self):
+        local = "'./data/comfy/*'"
+        target = f"'s3://{config.bucket}/comfy/{config.comfy_async_ep_name}/{id}/models/'"
+        logger.info(f"Syncing {local} to {target}")
+        os.system(f"rm -rf ./s5cmd")
+        os.system(f"wget -q ./ https://raw.githubusercontent.com/elonniu/s5cmd/main/s5cmd")
+        os.system(f"chmod +x ./s5cmd")
+        os.system(f"./s5cmd sync {local} {target}")
+
+    def test_2_sync_files(self):
+        headers = {
+            "x-api-key": config.api_key,
+            "username": config.username
+        }
+        data = {"endpoint_name": f"{config.comfy_async_ep_name}",
+                "need_reboot": True,
+                "prepare_id": id,
+                "prepare_type": "models"}
+        resp = self.api.prepare(data=data, headers=headers)
+        assert resp.status_code == 200, resp.dumps()
+        logger.info(resp.json())
+        logger.info(f"wait 30s for endpoint sync files...")
+        time.sleep(30)
 
     def test_1_comfy_txt2img_async_create(self):
         comfy_execute_create(1, self.api, config.comfy_async_ep_name)
