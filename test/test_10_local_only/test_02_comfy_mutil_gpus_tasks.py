af6d32ffe13256589882d7c925ec43a366c2cec6
Jingyi <elonniu@amazon.com>
2024-04-28 14:56:13 +0800

    improved process name

--- test/test_10_local_only/test_02_comfy_mutil_gpus_tasks.py
+++ test/test_10_local_only/test_02_comfy_mutil_gpus_tasks.py
@@ -72,25 +72,27 @@ class TestComfyMutilTaskGPUs:
         }
 
         while True:
-
-            resp = self.api.list_executes(headers=headers)
+            resp = self.api.list_executes(headers=headers, params={"limit": 20})
             executes = resp.json()['data']['executes']
             if len(executes) == 0:
                 break
 
+            execute_id_list = []
+            i = 0
             for execute in executes:
+                i = i + 1
                 prompt_id = execute['prompt_id']
-                data = {
-                    "execute_id_list": [
-                        prompt_id
-                    ],
-                }
-                resp = self.api.delete_executes(headers=headers, data=data)
-                logger.info(f"delete execute {prompt_id}")
-                if resp.status_code == 400:
-                    logger.info(resp.json()['message'])
-                    time.sleep(5)
-                    continue
+                execute_id_list.append(prompt_id)
+                logger.info(f"delete execute {i} {prompt_id}")
+
+            data = {
+                "execute_id_list": execute_id_list,
+            }
+            resp = self.api.delete_executes(headers=headers, data=data)
+            if resp.status_code == 400:
+                logger.info(resp.json()['message'])
+                time.sleep(5)
+                continue
 
     def test_14_comfy_gpus_start_async_tps(self):
         threads = []
@@ -110,4 +112,4 @@ class TestComfyMutilTaskGPUs:
 def create_batch_executes(n, api, endpoint_name):
     for i in range(n):
         comfy_execute_create(n=i, api=api, endpoint_name=endpoint_name, wait_succeed=False)
-        time.sleep(1)
+        time.sleep(5)
--- test/test_11_api_clean/test_07_clean_executes.py
+++ test/test_11_api_clean/test_07_clean_executes.py
@@ -26,22 +26,24 @@ class TestCleanExecutes:
         }
 
         while True:
-
-            resp = self.api.list_executes(headers=headers)
+            resp = self.api.list_executes(headers=headers, params={"limit": 20})
             executes = resp.json()['data']['executes']
             if len(executes) == 0:
                 break
 
+            execute_id_list = []
+            i = 0
             for execute in executes:
+                i = i + 1
                 prompt_id = execute['prompt_id']
-                data = {
-                    "execute_id_list": [
-                        prompt_id
-                    ],
-                }
-                resp = self.api.delete_executes(headers=headers, data=data)
-                logger.info(f"delete execute {prompt_id}")
-                if resp.status_code == 400:
-                    logger.info(resp.json()['message'])
-                    time.sleep(5)
-                    continue
+                execute_id_list.append(prompt_id)
+                logger.info(f"delete execute {i} {prompt_id}")
+
+            data = {
+                "execute_id_list": execute_id_list,
+            }
+            resp = self.api.delete_executes(headers=headers, data=data)
+            if resp.status_code == 400:
+                logger.info(resp.json()['message'])
+                time.sleep(5)
+                continue
