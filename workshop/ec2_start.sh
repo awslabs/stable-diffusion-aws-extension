934fdaa29d8c4cef6d5962d4a8d93bea96561850
Jingyi Niu <elonniu@amazon.com>
2024-05-21 17:23:18 +0800

    improved ec2 start

--- workshop/ec2_start.sh
+++ workshop/ec2_start.sh
@@ -46,6 +46,7 @@ docker tag "$image" "$release_image"
 aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin "$ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com"
 echo "docker push $release_image"
 docker push "$release_image"
+echo "docker pushed $release_image"
 
 mkdir -p ~/ComfyUI
 
