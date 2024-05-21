ae2135734f96237a268265ab4424d90e8c7a4c9d
Jingyi <elonniu@amazon.com>
2024-05-21 12:58:24 +0800

    fix permissions

--- workshop/ec2_start.sh
+++ workshop/ec2_start.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-export AWS_REGION=ap-northeast-1
 export ESD_VERSION='dev'
 export CONTAINER_NAME='comfy_ec2'
 
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin 366590864501.dkr.ecr.$AWS_REGION.amazonaws.com
-docker pull 366590864501.dkr.ecr.$AWS_REGION.amazonaws.com/esd-inference:$ESD_VERSION
-docker build -f Dockerfile.inference.from_scratch -t ec2-start .
+aws ecr get-login-password --region "$AWS_REGION" | docker login --username AWS --password-stdin 366590864501.dkr.ecr."$AWS_REGION".amazonaws.com
+docker pull 366590864501.dkr.ecr."$AWS_REGION".amazonaws.com/esd-inference:$ESD_VERSION
+docker build -f Dockerfile.comfy \
+             --build-arg ESD_VERSION='ec2' \
+             --build-arg S3_BUCKET_NAME='elonniu' \
+             --build-arg SERVICE_TYPE='comfy' \
+             --build-arg AWS_REGION='' \
+             --build-arg ON_EC2='true' \
+             --build-arg COMFY_API_URL="$COMFY_API_URL" \
+             --build-arg COMFY_API_TOKEN="$COMFY_API_TOKEN" \
+             --build-arg COMFY_ENDPOINT="$COMFY_ENDPOINT" \
+             --build-arg COMFY_NEED_SYNC="$COMFY_NEED_SYNC" \
+             --build-arg COMFY_BUCKET_NAME="$COMFY_BUCKET_NAME" \
+             -t ec2-start .
 
 docker rm "$CONTAINER_NAME" || true
 
