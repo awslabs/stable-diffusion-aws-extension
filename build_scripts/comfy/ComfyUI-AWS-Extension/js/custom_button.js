df8360ed36b820bd488087e54f2f5a44828be0f4
Xiujuan Li <xiujuali@amazon.com>
2024-07-04 16:29:50 +0800

    fix almost creat dialog in ui

--- build_scripts/comfy/ComfyUI-AWS-Extension/js/custom_button.js
+++ build_scripts/comfy/ComfyUI-AWS-Extension/js/custom_button.js
@@ -1310,8 +1310,7 @@ export class ModalTemplateDialog extends ComfyDialog{
 
     clear() {
         document.getElementById("input-template_field").value='';
-        document.getElementById("select-workflow_field").selected = '';
-        document.getElementById("select-workflow_field").value = '';
+        document.getElementById("select-workflow_field").value = 'default';
     }
 
     populateWorkflowSelectField() {
