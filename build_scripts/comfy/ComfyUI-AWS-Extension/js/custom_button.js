7c49012f127a4d174b3d6effe9281bde9d8b5f62
Xiujuan Li <xiujuali@amazon.com>
2024-07-12 10:45:47 +0800

    fix ui

--- build_scripts/comfy/ComfyUI-AWS-Extension/js/custom_button.js
+++ build_scripts/comfy/ComfyUI-AWS-Extension/js/custom_button.js
@@ -273,7 +273,7 @@ function createTemplateList() {
     title.textContent = 'Template List';
     title.style.fontSize = '14px';
     title.style.fontWeight = 'bold';
-    title.style.color = '#333';
+    title.style.color = '#b0b0b0';
     title.style.marginBottom = '0px';
     title.style.backgroundColor = '#f0f0f0';
     title.style.borderRadius = '4px';
